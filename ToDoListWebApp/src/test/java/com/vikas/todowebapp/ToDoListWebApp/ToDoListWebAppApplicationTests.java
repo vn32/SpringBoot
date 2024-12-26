5f1028283701a25c3f25dee2444d92945c2a67a5
vn1012 <nishadvikas87@gmai.com>
2024-12-26 18:37:42 +0530

    added restcontroller to say hello buddy

---
+++ ToDoListWebApp/src/test/java/com/vikas/todowebapp/ToDoListWebApp/ToDoListWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.todowebapp.ToDoListWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
