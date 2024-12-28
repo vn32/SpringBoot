e4e5a6a4b9b6ad44da69c7892fcdb1821e9ee9e6
vn1012 <nishadvikas87@gmai.com>
2024-12-28 18:24:24 +0530

    resolved issues with jsp not loading by adding jasper dependency

--- ToDoListWebApp/src/main/java/com/vikas/todowebapp/ToDoListWebApp/hello/SayHelloController.java
+++ ToDoListWebApp/src/main/java/com/vikas/todowebapp/ToDoListWebApp/hello/SayHelloController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class SatHelloController {
+public class SayHelloController {
 
     @RequestMapping("/say-hello")
     @ResponseBody
     public String sayHello(){
         return "Hi! Keep Learning";
+    }
+
+//to view the jsp, add the prefix and suffix in application.properties file and add tomcat embed dependency in pom.xml
+    @RequestMapping("/say-hello-jsp")
+    public String sayHelloJSP(){
+        return "sayHello";
     }
 
 }
