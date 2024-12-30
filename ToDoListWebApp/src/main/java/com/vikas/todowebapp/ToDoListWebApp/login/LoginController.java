f1232d7c403cfc32ea9982984629b499e32a2908
vn1012 <nishadvikas87@gmai.com>
2024-12-30 17:53:31 +0530

    Added a welcome page after succesfull login

--- ToDoListWebApp/src/main/java/com/vikas/todowebapp/ToDoListWebApp/login/LoginController.java
+++ ToDoListWebApp/src/main/java/com/vikas/todowebapp/ToDoListWebApp/login/LoginController.java
@@ -1,19 +1,44 @@
 package com.vikas.todowebapp.ToDoListWebApp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    //add a logger
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     //http://localhost:8080/login?name=Vikas, handling request parameters by @RequestParam
-    @RequestMapping("/login")
-    public String gotoLoginPage(@RequestParam String name, Model model) {
-        //How we will pass the request parameters value into the jsp or view page, that's why model is used
-        model.addAttribute("name",name);
+//    @RequestMapping("/login")
+//    public String gotoLoginPage(@RequestParam String name, Model model) {
+//        //How we will pass the request parameters value into the jsp or view page, that's why model is used
+//        model.addAttribute("name",name);
+//        logger.debug("Request param is {} "+name);
+//        return "login";
+//    }
+
+    //by default it's accepting the get method
+    @RequestMapping(value="/login",method= RequestMethod.GET)
+    public String gotoLoginPage() {
+        return "login";
+    }
+
+    //As in the login.jsp page, form method is post, so request is Post method handled in below method
+    //form data will be coming as request and can be mapped using request parameter
+    @RequestMapping(value="/login",method= RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name,@RequestParam String password,Model model) {
+        //let's check for the dummy username and password
+        //username:vikas, password:dummy
+        if(name.equals("vikas") && password.equals("dummy")){
+            model.addAttribute("name",name);
+            return "welcome";
+        }
         return "login";
     }
 
--- ToDoListWebApp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ ToDoListWebApp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -9,7 +9,16 @@ pageEncoding="ISO-8859-1"%>
 </head>
 <body>
 <p> Welcome to Login Page! </p>
-using expression language
-Hello Mr. ${name}
+<form method="post">
+  <div class="container">
+    <label for="name"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="name" required>
+
+    <label for="password"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+
+    <button type="submit">Login</button>
+  </div>
+</form>
 </body>
 </html>
